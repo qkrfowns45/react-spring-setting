7c9124fa8652db5d766b9b2580198f4535aea2df
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-01-13 20:51:14 +0900

    junit 단위테스트 및 통합테스트(Mockito 패턴)

--- book/src/test/java/com/newbietop/book/web/BookControllerIntegreTest.java
+++ book/src/test/java/com/newbietop/book/web/BookControllerIntegreTest.java
@@ -1,14 +1,29 @@
 package com.newbietop.book.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newbietop.book.domain.Book;
+
+import lombok.extern.slf4j.Slf4j;
 
 //통합테스트(모든 Bean들을 똑같이 Ioc 올리고 테스트 하는 것)
 
+@Slf4j
 @Transactional //각각의 테스트함수가 종료될때마다 롤백해주는 트랜잭션이다.
 @AutoConfigureMockMvc //통합테스트에선 이게 있어야 ioc에 등록해줌
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)//Mock은 실제 톰켓을 올리는게 아니라 다른 톰켓으로 테스트
@@ -16,4 +31,23 @@ public class BookControllerIntegreTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
+
+	//BDD mockito패턴 given when then
+	@Test
+	public void save_테스트() throws Exception {
+		//given 테스트를 하기 위한 준비 
+		
+		Book book = new Book(null,"스프링 따라하기","newbietop"); //던질데이터 준비(given)
+		
+		String content = new ObjectMapper().writeValueAsString(book); //json으로 변환해서 나타나게 해준다.
+		
+		// when (테스트 실행)
+		ResultActions resultActions = mockMvc.perform(post("/book")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then (검증)
+		resultActions.andExpect(status().isCreated()).andExpect(jsonPath("$.title").value("스프링 따라하기")).andDo(MockMvcResultHandlers.print());
+	}
 }
--- book/src/test/java/com/newbietop/book/web/BookControllerUnitTest.java
+++ book/src/test/java/com/newbietop/book/web/BookControllerUnitTest.java
@@ -1,10 +1,55 @@
 package com.newbietop.book.web;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.newbietop.book.domain.Book;
+import com.newbietop.book.service.BookService;
+
+import lombok.extern.slf4j.Slf4j;
 
 //단위테스트(controller만 테스트)Filter, ControllerAdvice
 
+@Slf4j
 @WebMvcTest//spring에서 class확장이 필요하다 @WebMvcTest안에 @ExtendWith(SpringExtension.class)가 없으면 붙여줘야한다.Junit5에서는 포함되어 있지만 4에는 없다
 public class BookControllerUnitTest {
-
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean //ioc환경에 bean 등록된다. controller환경에 service는 올라오지 않으니 강제로 가짜를 주입하는거다.
+	private BookService bookService; 
+	
+	//BDD mockito패턴 given when then
+	@Test
+	public void save_테스트() throws Exception {
+		//given 테스트를 하기 위한 준비 
+		
+		Book book = new Book(null,"스프링 따라하기","newbietop"); //던질데이터 준비(given)
+		
+		String content = new ObjectMapper().writeValueAsString(book); //json으로 변환해서 나타나게 해준다.
+		when(bookService.저장하기(book)).thenReturn(new Book(1L,"스프링 따라하기","newbietop")); //stub!
+		
+		// when (테스트 실행)
+		ResultActions resultActions = mockMvc.perform(post("/book")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(content)
+				.accept(MediaType.APPLICATION_JSON_UTF8));
+		
+		//then (검증)
+		resultActions.andExpect(status().isCreated()).andExpect(jsonPath("$.title").value("스프링 따라하기")).andDo(MockMvcResultHandlers.print());
+	}
 }
